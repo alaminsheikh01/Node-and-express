10a7dd50c9807aa4e0f8216cb9010a12e0893c47
alamin <alamin@gmail.com>
2020-12-26 18:56:02 +0600

    2nd day commit

--- Route.js
+++ Route.js
@@ -2,7 +2,11 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res) {
-    res.send('Hello world');
+    res.send('Welcome to our homepage');
+})
+
+app.route('/about').get(function(req, res) {
+    res.send('welcome to about us page');
 })
 
 var server = app.listen(3000, function() {
